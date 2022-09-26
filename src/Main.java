e239c1c9e432eac93bcbae3053a12c03f92c9124
Paul Connolly <paulconn5@gmail.com>
2022-09-25 22:56:39 -0400

    Finished task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double itemPrice = 110;
+        double totalCost = itemPrice;
+        double shippingCost = 0;
+        System.out.println("Item price set at $" + itemPrice);
+
+        if (itemPrice <= 100) {
+            shippingCost = 0.02 * itemPrice;
+        }
+        totalCost = itemPrice + shippingCost;
+        System.out.println("Item price: $" + itemPrice + " Shipping Cost: $" + shippingCost +
+                " Total Cost: " + totalCost);
     }
 }
